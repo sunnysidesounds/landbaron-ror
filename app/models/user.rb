489713b8f63b14bf980057de8b27c2120d89ca05
Jason R Alexander <sunnysidesounds@gmail.com>
2015-02-23 14:33:45 -0800

    Built out user model
    [#88872338]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,49 @@
 class User < ActiveRecord::Base
   has_many :orders
-end
+  # attr_accessible :email, :username, :password, :password_confirmation
+  attr_accessor :password
+  before_save :encrypt_password
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email, :on => :create
+  validates_presence_of :username, :on => :create
+  validates_uniqueness_of :email
+  validates_uniqueness_of :username
+
+
+  def initialize(attributes = {})
+    super # must allow the active record to initialize!
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+
+  def self.authenticate_by_email(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def self.authenticate_by_username(username, password)
+    user = find_by_username(username)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+
+  end

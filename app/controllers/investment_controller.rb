efb956c653786628315e04f27f42a3bbe0a84180
Jason R Alexander <sunnysidesounds@gmail.com>
2015-03-16 06:53:26 -0700

    Working on quote controller action
    [#89773228]

--- app/controllers/investment_controller.rb
+++ app/controllers/investment_controller.rb
@@ -26,6 +26,9 @@ class InvestmentController < ApplicationController
     if params[:id]
       @investments = Investment.new
       @invest_now = @investments.get_investment_details(params[:id])
+      @user = current_user
+      # TODO: If we start using reference_id
+      @reference_id = "uid#{current_user.id}ln#{current_user.last_name}iid#{params[:id]}"
     end
   end
 
@@ -34,6 +37,27 @@ class InvestmentController < ApplicationController
       @investments = Investment.new
       @confirmation = @investments.get_investment_details(params[:id])
     end
+  end
+
+  def create_quote
+    @quote = Quote.new(quote_params)
+
+    # Stoppped here need to see what to pass
+    render :action => "new_quote"
+
+
+  #  if quote_params[:user_id] == current_user.id
+   #   flash.now[:error] = 'User match'
+   # end
+
+
+
+
+    end
+
+
+  def quote_params
+    params.require(:quote).permit(:reference_id, :user_id, :total_amount, :investor_profile_type, :status, :payment_methd)
   end
 
 

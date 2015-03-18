37fbaecbb8d55a9721e50fffb5b4ac61ff30c8e5
James Vastbinder <james.vastbinder@gmail.com>
2015-03-18 16:40:32 -0700

    mailer configured for gmail
    [#90656040]

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,2 +1,9 @@
 class ConfirmationMailer < ApplicationMailer
+    default from: "james@landbaronclub.com"
+
+    def confirmation_email(user, quote)
+        @user = user
+        @quote = quote
+        mail(to: @user.email, subject: 'Land Baron Club Investment Confirmation')
+    end
 end

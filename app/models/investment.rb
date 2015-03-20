dae71b3887114304ef6f919cc68b60c4f9593337
Jason R Alexander <sunnysidesounds@gmail.com>
2015-03-19 21:00:01 -0700

    Set investment order by id
    [#89773228]

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,12 +2,12 @@ class Investment < ActiveRecord::Base
 
   def get_landing_page_investments(status)
     limit = status == 'past' ? '8': '3'
-    sql = "SELECT * FROM investments WHERE status='"+status+"' LIMIT "+limit+" "
+    sql = "SELECT * FROM investments WHERE status='"+status+"' ORDER BY id LIMIT "+limit+" "
     @list = ActiveRecord::Base.connection.execute(sql)
   end
 
   def get_investment_details(id)
-    sql = "SELECT * FROM investments WHERE id='"+id+"'"
+    sql = "SELECT * FROM investments WHERE id='"+id+"' ORDER BY id"
     @list = ActiveRecord::Base.connection.execute(sql)
   end
 

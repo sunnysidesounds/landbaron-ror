a9ed9ba5f8e4d7b82d75860bd305bc00aec93165
James Vastbinder <james.vastbinder@gmail.com>
2015-04-05 03:41:16 -0400

    fix syntax error and use ruby wrapper
    [#91813428]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -42,11 +42,12 @@ server '192.241.207.43', user: 'deploy', port: 22, password: fetch(:password), r
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+    user: 'deploy',
+    keys: %w(/home/deploy/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------

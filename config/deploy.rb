c93b63d137b0790253f1c64dc9b2cc9bdc369b1d
James Vastbinder <james.vastbinder@gmail.com>
2015-04-18 16:43:32 -0700

    updated deploy.rb for puma
    [#91813428]

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,9 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
-
-set :application, 'landbaron'
-set :scm, :git
-set :repo_url, 'git@github.com:landbaronclub/landbaron.git'
-
-# Deploy to directory
-set :deploy_to, '/home/deploy/landbaron'
-
-set :pty, true
-set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-    desc 'Restart Application'
-    task :restart do
-        on roles(:app), in: :sequence, wait: 5 do
-            execute :touch, release_path.join('tmp/restart.txt')
-        end
-    end
-
-    after :publishing, 'deploy:restart'
-    after :finishing, 'deploy:cleanup'
-end
-
 # Change these
-server 'server', port: 7171, roles: [:web, :app, :db], primary: true
+server 'ww2.landbaronclub.com', roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@example.com:user/app.git'
-set :application,     'appname'
-set :user,            'username'
+set :repo_url,        'git@github.com:landbaronclub/landbaron.git'
+set :application,     'landbaron'
+set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -53,7 +12,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -62,7 +21,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -120,11 +79,6 @@ namespace :deploy do
     after  :finishing,    :restart
 end
 
-                                                                                                                )
-                                                                                      )"
-                                    )"
-                    }"
-              }"
-        )
-}}
-)
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma"

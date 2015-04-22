bd9227d1439090a50cc16d60518eaf05e6e8fef3
James Vastbinder <james.vastbinder@gmail.com>
2015-04-21 18:43:26 -0700

    clean up sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,11 @@
 //= require preorder
 //= require angular.min
 //= require angular-ui-router
+//= require ui-bootstrap-tpls-0.12.0
 //= require jquery.bxslider
 //= require jquery.validate
 //= require additional-methods
 //= require jquery.maskedinput
-//= require bootstrap
-//= require ui-bootstrap-tpls-0.12.0
-//= require_tree .
 
 
 $(document).ready(function() {

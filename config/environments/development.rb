6535ebd97fbd1c9be3a72fd67fb979aa7672874d
Jason R Alexander <sunnysidesounds@gmail.com>
2015-04-21 22:52:14 -0700

    db

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,10 @@ LandBaron::Application.configure do
   config.action_mailer.smtp_settings = {
       :address                  => 'smtp.gmail.com',
       :port                     => 587,
-      :user_name                => ENV['GMAIL_USER'],
-      :password                 => ENV['GMAIL_PASS'],
+     # :user_name                => ENV['GMAIL_USER'],
+     # :password                 => ENV['GMAIL_PASS'],
+      :user_name                => 'investor@landbaronclub.com',
+      :password                 => 'lbc2015z0e',
       :authentication           => 'plain',
       :enable_starttls_auto     => true
   }

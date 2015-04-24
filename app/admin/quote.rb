5a88bca16fddfd76d3f9c81a0806f004e4a85e10
Jason R Alexander <sunnysidesounds@gmail.com>
2015-04-23 22:51:41 -0700

    Completed updates to active admin quotes

--- app/admin/quote.rb
+++ app/admin/quote.rb
@@ -27,8 +27,71 @@ ActiveAdmin.register Quote do
   end
 
 
+  # Will display the image on show object page
+  show do |ad|
+    user_info = User.new
+    investment = Investment.new
+    u = user_info.get_user(quote.user_id)
+    inv = investment.get_a_investment_detail(quote.investment_id)
 
 
+    attributes_table do
+      row :id
+      row :reference_id
+      row :user_id do
+        link_to u.first_name + " " + u.last_name, admin_quote_path(quote)
+      end
+      row 'Investment' do
+        link_to inv.name, admin_investment_path(quote.investment_id)
+      end
+      row :total_amount
+      row :investor_profile_type
+
+      row 'Payment Method' do
+        :payment_methd
+      end
+
+
+      row :status
+
+    end
+  end
+
+
+
+  # Detail view / form
+  form :html => { :enctype => 'multipart/form-data' } do |f|
+    f.inputs "Quote Details" do
+
+      user_info = User.new
+      investment = Investment.new
+      u = user_info.get_user(quote.user_id)
+      inv = investment.get_a_investment_detail(quote.investment_id)
+
+
+      f.input :user_id, :input_html => { :disabled => true } ,  :label => u.first_name + " " + u.last_name + ' ID'
+      f.input :investment_id, :input_html => { :disabled => true } ,  :label => inv.name + " ID"
+      f.input :total_amount, :required => true, :label => 'Investment Amount'
+      f.input :investor_profile_type, :as => :select,
+              :collection => [['Entity', 'entity'],
+                              ['Myself','myself'],
+                              ['IRA', 'ira']],
+              :required => true,
+              :include_blank => false
+      f.input :payment_methd, :as => :select,
+              :collection => [['Transfer', 'transfer'],
+                              ['Check','check']],
+              :required => true,
+              :include_blank => false
+      f.input :status, :as => :select,
+              :collection => [['Pending', 'pending'],
+                              ['Completed','completed']],
+              :required => true,
+              :include_blank => false
+      f.submit 'Submit', :class => 'active_admin_button'
+      f.cancel_link('/admin/quote')
+    end
+  end
 
 
   # Redirect to list view

2ead33534b531e824a4fac7d8636e45c54602b7a
Arsalan Tahir <atahir@csquareonline.com>
2015-06-07 01:00:43 +0500

    Commented old code for current_user and authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
-  before_filter :authenticate_user, :only => [:investment, :investment_detail, :preorder, :account]
+  # helper_method :current_user
+  before_filter :authenticate_user!
+  skip_before_filter  :verify_authenticity_token
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # def current_user
+  #   # Note: we want to use "find_by_id" because it's OK to return a nil.
+  #   # If we were to use User.find, it would throw an exception if the user can't be found.
+  #   @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  # end
 
-  def current_user
-    # Note: we want to use "find_by_id" because it's OK to return a nil.
-    # If we were to use User.find, it would throw an exception if the user can't be found.
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
+  # def authenticate_user
+  #   if current_user.nil?
+  #     flash[:error] = 'Please sign in to view this page!'
+  #     redirect_to :root
+  #   end
+  # end
 
-  def authenticate_user
-    if current_user.nil?
-      flash[:error] = 'Please sign in to view this page!'
-      redirect_to :root
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :phone_number, :address, :city, :state, :postal_code, :income_range, :username, :email, :password, :password_confirmation, :principle_investor, :verified, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 end

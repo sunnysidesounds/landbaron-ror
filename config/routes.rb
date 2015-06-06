3d5824dc73d51205146155d7f8bf44fba2f54974
Arsalan Tahir <atahir@csquareonline.com>
2015-06-07 01:05:23 +0500

    Route change for devise and devise initializer change

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,19 @@ LandBaron::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root :to => 'authentication#sign_in'
+  devise_for :users
+  
+  devise_scope :user do
+    authenticated :user do
+      root 'investments#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
+  # root :to => "devise/sessions#new"
 
   # Deal Room
   match '/preorder'               => 'preorder#index', :via => [:get,:post]
@@ -14,26 +26,26 @@ LandBaron::Application.routes.draw do
   match '/preorder/postfill'      => 'preorder#postfill', :via => [:get,:post]
 
   # Authentication
-  get 'sign_in' => 'authentication#sign_in'
-  post 'sign_in' => 'authentication#login'
-  get 'signed_out' => 'authentication#signed_out'
-  get 'change_password' => 'authentication#change_password'
-  get 'forgot_password' => 'authentication#forgot_password'
-  get 'new_user' => 'authentication#new_user'
-  post 'new_user' => 'authentication#register'
-  get 'password_sent' => 'authentication#password_sent'
+  # get 'sign_in' => 'authentication#sign_in'
+  # post 'sign_in' => 'authentication#login'
+  # get 'signed_out' => 'authentication#signed_out'
+  # get 'change_password' => 'authentication#change_password'
+  # get 'forgot_password' => 'authentication#forgot_password'
+  # get 'new_user' => 'authentication#new_user'
+  # post 'new_user' => 'authentication#register'
+  # get 'password_sent' => 'authentication#password_sent'
 
   # My Account
   get 'account' => 'my_account#account'
   #Landing Page
-  get 'investments' => 'investment#investment'
-  match 'investment_detail/:id'   => 'investment#investment_detail', :via => :get
-  match 'invest/:id'   => 'investment#investment_now', :via => :get
+  get 'investments' => 'investments#investment'
+  match 'investment_detail/:id'   => 'investments#investment_detail', :via => :get, as: 'investment_detail'
+  match 'invest/:id'   => 'investments#investment_now', :via => :get, as: 'new_invest'
 
   # quote
   post 'quote' => 'quote#create_quote'
   # confirmation
-  match 'confirmation/:id'   => 'investment#investment_confirmation', :via => :get
+  match 'confirmation/:id'   => 'investments#investment_confirmation', :via => :get
 
 
   # votes

18e2657d3903316d708d103d6625a478e4b41c2d
Arsalan Tahir <atahir@csquareonline.com>
2015-06-18 22:19:38 +0500

    Fix for admin user login failure.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,6 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Land Baron Club"
+  config.skip_before_filter :authenticate_user!
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.

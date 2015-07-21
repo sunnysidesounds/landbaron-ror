232cb9a1d1a7edd67cf9109a1fe77699729440e1
Arsalan Tahir <atahir@csquareonline.com>
2015-07-21 17:14:44 +0500

    Adding changes for js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.textchange
-
-// Removed due to b.browser undefined
-// require jquery.details
-
 //= require preorder
 //= require jquery.bxslider
 //= require jquery.validate
@@ -27,6 +23,42 @@
 //= require jquery.formatCurrency
 //= require_tree .
 
+
+
+var matched, browser;
+
+jQuery.uaMatch = function( ua ) {
+    ua = ua.toLowerCase();
+
+    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        [];
+
+    return {
+        browser: match[ 1 ] || "",
+        version: match[ 2 ] || "0"
+    };
+};
+
+matched = jQuery.uaMatch( navigator.userAgent );
+browser = {};
+
+if ( matched.browser ) {
+    browser[ matched.browser ] = true;
+    browser.version = matched.version;
+}
+
+// Chrome is Webkit, but Webkit is also Safari.
+if ( browser.chrome ) {
+    browser.webkit = true;
+} else if ( browser.webkit ) {
+    browser.safari = true;
+}
+
+jQuery.browser = browser;
 
 
 $(document).ready(function(){

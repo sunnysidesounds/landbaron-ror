f60d84233de6804ca577855f6cf0cbe9e2743903
Arsalan Tahir <azlantaher@gmail.com>
2015-10-21 01:38:29 +0500

    Allowing images to be served staticaly which are used for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ LandBaron::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.serve_static_files = true
   config.assets.precompile += %w( application.js application.css  )
 
   # Disable delivery errors, bad email addresses will be ignored
